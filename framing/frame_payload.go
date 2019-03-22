7155bb05d338f1f100f9df9350e4cc39dcc585f3
linux_china <libing.chen@gmail.com>
2019-03-22 11:39:54 -0700

    implement HasMetadata() for FramePayload

--- framing/frame_payload.go
+++ framing/frame_payload.go
@@ -29,6 +29,11 @@ func (p *FramePayload) Data() []byte {
 	return p.trySliceData(0)
 }
 
+// Returns whether the payload has metadata
+func (p *FramePayload) HasMetadata() bool {
+	return len(p.Data()) > 0
+}
+
 // MetaData returns as utf-8 string
 func (p *FramePayload) GetMetadataUtf8() string {
 	return string(p.Metadata())
