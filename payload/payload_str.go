79b3f1c19b60ed533b4b5a3ee7f96b282e3dae13
Jeffsky <jjeffcaii@outlook.com>
2019-03-29 09:49:27 +0800

    add func 'payload.NewFile' which can be used to create Payload from a local file.

---
+++ payload/payload_str.go
@@ -0,0 +1,36 @@
+package payload
+
+import "fmt"
+
+type strPayload struct {
+	data     string
+	metadata string
+}
+
+func (p *strPayload) String() string {
+	return fmt.Sprintf("Payload{data=%s,metadata=%s}", p.data, p.metadata)
+}
+
+func (p *strPayload) Metadata() (metadata []byte, ok bool) {
+	ok = len(p.metadata) > 0
+	if ok {
+		metadata = []byte(p.metadata)
+	}
+	return
+}
+
+func (p *strPayload) MetadataUTF8() (metadata string, ok bool) {
+	return p.metadata, len(p.metadata) > 0
+}
+
+func (p *strPayload) Data() []byte {
+	return []byte(p.data)
+}
+
+func (p *strPayload) DataUTF8() string {
+	return p.data
+}
+
+func (*strPayload) Release() {
+	// ignore
+}
