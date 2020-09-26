ffaf72e0f6008ed1bd00193d36c25621e6193a7d
Jeffsky <jjeffcaii@outlook.com>
2020-09-26 10:26:52 +0800

    fix ut.

--- rsocket_test.go
+++ rsocket_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/jjeffcaii/reactor-go"
+	"github.com/jjeffcaii/reactor-go/scheduler"
 	"github.com/pkg/errors"
 	. "github.com/rsocket/rsocket-go"
 	"github.com/rsocket/rsocket-go/core/transport"
@@ -375,23 +376,34 @@ func testAll(t *testing.T, proto string, clientTp transport.ClientTransporter, s
 					RequestChannel(func(inputs rx.Publisher) flux.Flux {
 						//var count int32
 						//countPointer := &count
+						done := make(chan struct{})
 						receives := make(chan payload.Payload)
+						receiveErrors := make(chan error)
 
 						go func() {
-							var count int32
-							for range receives {
-								count++
+							success := new(int32)
+							failed := new(int32)
+
+						L:
+							for {
+								select {
+								case <-done:
+									break L
+								case <-receives:
+									atomic.AddInt32(success, 1)
+								case <-receiveErrors:
+									atomic.AddInt32(failed, 1)
+								}
 							}
-							assert.Equal(t, channelElements, count, "bad channel amount")
+							assert.Equal(t, channelElements, atomic.LoadInt32(success)+atomic.LoadInt32(failed))
 						}()
 
-						inputs.(flux.Flux).DoFinally(func(s rx.SignalType) {
-							close(receives)
-						}).Subscribe(context.Background(), rx.OnNext(func(input payload.Payload) error {
-							//fmt.Println("rcv from channel:", input)
-							receives <- input
-							return nil
-						}))
+						inputs.(flux.Flux).
+							DoFinally(func(s rx.SignalType) {
+								close(done)
+							}).
+							SubscribeOn(scheduler.Parallel()).
+							SubscribeWithChan(context.Background(), receives, receiveErrors)
 
 						return flux.Create(func(ctx context.Context, s flux.Sink) {
 							for i := 0; i < int(channelElements); i++ {
